39ce810fa6917292e933052339ab5f58ddba96cf
miffi <michaelciociola123@gmail.com>
2016-11-30 12:41:07 -0500

    Initial Commit

---
+++ TemplateMatching/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TemplateMatching
+//
+//  Created by Michael on 11/30/16.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
